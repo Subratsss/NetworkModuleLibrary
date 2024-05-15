d04fead8c8aea3d69126128ef623a066d333d069
Wishtechnica <wishtechnica@gmail.com>
2024-05-15 15:02:47 +0530

    move retrofit related dependencies in to network module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,10 +60,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
-    implementation(libs.retrofit)
-    implementation(libs.gson)
-    implementation(libs.okhttp)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- networkmodule/build.gradle.kts
+++ networkmodule/build.gradle.kts
@@ -37,6 +37,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    implementation(libs.retrofit)
+    implementation(libs.gson)
+    implementation(libs.okhttp)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
